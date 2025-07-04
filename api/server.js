1e4e28ee171bfa280c54f980d13ffe9654b2965c
Kadockh <andreykadoch20@gmail.com>
2025-07-04 01:23:53 -0300

    feat: add CORS support on json server

--- api/server.js
+++ api/server.js
@@ -11,6 +11,19 @@ const router = jsonServer.router(db);
 
 const middlewares = jsonServer.defaults();
 
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://task-manager-phi-jade-95.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
+
 server.use(middlewares);
 server.use(
     jsonServer.rewriter({
